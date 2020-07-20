be1c969c95246d655a7f92d5458559c76bcc36c2
Rhys Perry <rhysperry111@gmail.com>
2020-07-20 19:47:41 +0200

    Delete mousebat.sh

--- .config/waybar/mousebat.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-dbus-send --print-reply=literal --system --dest=org.bluez /org/bluez/hci0/dev_F2_AD_70_FC_E6_FD org.freedesktop.DBus.Properties.Get string:"org.bluez.Battery1" string:"Percentage" | cut -d ' ' -f12
